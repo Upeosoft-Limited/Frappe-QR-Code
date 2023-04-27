74fef6d863c4e9324eb2c548d98c87f6dd6dac42
Verckys Orwa <vrcxabayo@gmail.com>
2023-04-27 15:40:20 +0300

    Changed Settings doctype to a single doctype

--- frappe_qrcode/frappe_qr_code/doctype/qr_code_settings/qr_code_settings.js
+++ frappe_qrcode/frappe_qr_code/doctype/qr_code_settings/qr_code_settings.js
@@ -2,19 +2,7 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('QR Code Settings', {
-	validate: function(frm) {
-		frappe.call({
-			method: 'frappe_qrcode.services.rest.generate_qr_code',
-			args: {
-				"box_size":frm.doc.box_size,
-				"border":frm.doc.border,
-				"fill_color":frm.doc.fill_color,
-				"back_color":frm.doc.back_color
-				},
-			callback:function (r) {
-				let succes_message=r.message
-				console.log(succes_message)
-			}
-		});
-	}
+	// refresh: function(frm) {
+
+	// }
 });
